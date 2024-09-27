f2d7682651f443c5b5c6d4e43b823f68e3614c06
suemoron <susumoron@hotmail.com>
2024-09-26 22:04:58 -0300

    criei a pasta contatos

---
+++ src/app/componentes/contatos/page.jsx
@@ -0,0 +1,11 @@
+import style from './contatos.module.css'
+
+export default function Contatos(){
+    return(
+        <>
+        <div className={style.eu}></div>
+        
+        
+        </>
+    )
+}

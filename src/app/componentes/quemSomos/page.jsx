d53bffe30ebc4d1f5ed11b667dff87fd56542e05
wellmendes25 <wellyson.wm@gmail.com>
2025-01-15 20:40:18 -0300

    puxar alterações do quem somos novamente

--- src/app/componentes/quemSomos/page.jsx
+++ src/app/componentes/quemSomos/page.jsx
@@ -1,16 +1,1 @@
 // pages/quem-somos.js
-import React from 'react';
-import estilos from './quemSomos.module.css';
-
-export default function QuemSomos() {
-  return (
-   <section className={estilos.section}>
-    <div>
-
-    </div>
-   </section>
-    
-  );
-};
-
-

887be9a921de30f56c0c5159f9dda6b42168874a
wellmendes25 <wellyson.wm@gmail.com>
2025-01-20 21:46:48 -0300

    adicionando lib gsap

--- src/app/page.js
+++ src/app/page.js
@@ -1,51 +1,113 @@
 'use client'
-import Image from "next/image";
+import { useRef, useEffect } from "react";
 import estilos from "./page.module.css";
-
-import Caridade3 from '../../public/caridade.jpg'
 import Videosrc from '../../public/video1.mp4'
 import Carousel from "./componentes/carousel/page";
-
-
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 
 export default function Home() {
+  const refs= {
+    title: useRef(null),
+    section: useRef(null),
+    title2: useRef(null),
+    section2: useRef(null),
+    divs: [
+      useRef(null),
+      useRef(null),
+      useRef(null),
+      useRef(null),
+    ],
+
+  }
+
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(()=>{
+
+    const { title, section, title2, section2, divs } = refs;
+
+    const timeLine = gsap.timeline({defaults: {duration: 1}})
+
+    gsap.fromTo(title.current, 
+      {opacity: 0, x: "-500px",},
+      {opacity: 1, x: "0px", duration: 1, scrollTrigger: {trigger: section.current, start: "top 10%", end: "top -20%",scrub: true}
+  })
+
+  gsap.fromTo(title2.current, 
+    {opacity: 0,x: "-500px",},
+    {opacity: 1,x: "0px",duration: 1,scrollTrigger: {trigger: section2.current,start: "top 80%", end: "top 60%", scrub: true}
+})
+
+ divs.forEach((div, index) => {
+  timeLine.fromTo(div.current, { opacity: 0, x: "-500px" },
+    { opacity: 1, x: "0px" 
+          
+    })
+ })
+
+  }, [])
+
   return (
     <main>
-      <section className={estilos.main_container}>
-        <video className={estilos.video_main} alt="video de pessoas caridosas" src={Videosrc} autoPlay muted loop></video>
-        <h1>Leo Clube sertões</h1>
+      <section ref={refs.section} className={estilos.main_container}>
+        <video
+          className={estilos.video_main}
+          alt="video de pessoas caridosas"
+          src={Videosrc}
+          autoPlay
+          muted
+          loop
+        ></video>
+        <h1 ref={refs.title}>
+          Leo Clube Sertões
+        </h1>
       </section>
 
-      <section className={estilos.container_section}>
-        <h1>Conheça-nos</h1>
-        
-            
-            <div className={estilos.in}>
-              <div className={estilos.div_in_section}>
-              
-                <div className={estilos.container_p}>
-                  <h2>Em que acreditamos:</h2>
-                  <p>Acreditamos que, ao unir nossas forças e corações, podemos criar um mundo onde a solidariedade e a compaixão guiem nossas ações, transformando vidas e construindo um futuro mais justo para todos.</p>
-                </div>
-                <div className={estilos.container_p}>
-                  <h2>Nossas particularidade:</h2>
-                  <p>Em cada pequeno gesto de bondade, encontramos o poder de mudar não apenas o dia de alguém, mas também o curso de suas vidas, mostrando que juntos podemos superar qualquer desafio.</p>
-                </div>
-                <div className={estilos.container_p}>
-                  <h2>Nossa missão: </h2>
-                  <p>Nossa missão é ser a luz na escuridão, oferecendo apoio e esperança àqueles que mais precisam, sabendo que o impacto de nossas ações hoje pode ressoar por gerações futuras.</p>
-                </div>
-                <div className={estilos.container_p}>
-                  <h2>Quando somos um só:</h2>
-                  <p>Quando estendemos a mão ao próximo, descobrimos que a verdadeira força está na união e na capacidade de criar um legado de amor e compaixão que transformará o mundo ao nosso redor."</p>
-                </div>
-              
-              </div>
-            </div>   
-          
+      <section ref={refs.section2} className={estilos.container_section}>
+        <h1 ref={refs.title2}>Conheça-nos</h1>
 
-          <h1>Algumas de nossas atividades:</h1>
-          <div className={estilos.carousel}><Carousel/></div>
+        <div className={estilos.in}>
+          <div className={estilos.div_in_section}>
+            <div ref={refs.divs[0]} className={estilos.container_p}>
+              <h2>Em que acreditamos:</h2>
+              <p>
+                Acreditamos que, ao unir nossas forças e corações, podemos criar
+                um mundo onde a solidariedade e a compaixão guiem nossas ações,
+                transformando vidas e construindo um futuro mais justo para
+                todos.
+              </p>
+            </div>
+            <div ref={refs.divs[1]} className={estilos.container_p}>
+              <h2>Nossas particularidade:</h2>
+              <p>
+                Em cada pequeno gesto de bondade, encontramos o poder de mudar
+                não apenas o dia de alguém, mas também o curso de suas vidas,
+                mostrando que juntos podemos superar qualquer desafio.
+              </p>
+            </div>
+            <div ref={refs.divs[2]} className={estilos.container_p}>
+              <h2>Nossa missão: </h2>
+              <p>
+                Nossa missão é ser a luz na escuridão, oferecendo apoio e
+                esperança àqueles que mais precisam, sabendo que o impacto de
+                nossas ações hoje pode ressoar por gerações futuras.
+              </p>
+            </div>
+            <div ref={refs.divs[3]} className={estilos.container_p}>
+              <h2>Quando somos um só:</h2>
+              <p>
+                Quando estendemos a mão ao próximo, descobrimos que a verdadeira
+                força está na união e na capacidade de criar um legado de amor e
+                compaixão que transformará o mundo ao nosso redor.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <h1>Algumas de nossas atividades:</h1>
+        <div className={estilos.carousel}><Carousel /></div>
       </section>
     </main>
   );
